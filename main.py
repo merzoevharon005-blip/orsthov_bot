cf3348a30ea603680a2f9548c1b018fd0664ae45
merzoevharon005-blip <merzoevharon005@gmail.com>
2026-08-29 14:41:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import asyncio
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = "8275887470:AAHckltQmADi9oLlNTPFmSHxchgzGFwH9qQ"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Привет! Я работаю! 🤖')
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+async def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    
+    print("✅ Бот запущен!")
+    await app.run_polling()
 
-print("✅ Бот запущен!")
-app.run_polling()
+if __name__ == "__main__":
+    asyncio.run(main())
